82d3d9bdadfdb74286d764f4383bad4509b6ddef
celsowm <celsowm@gmail.com>
2026-08-08 08:20:41 -0300

    feat: add public umbrella header

---
+++ include/metal/metal.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "metal/ddl.hpp"
+#include "metal/execution.hpp"
+#include "metal/mapping.hpp"
+#include "metal/orm.hpp"
+#include "metal/query.hpp"
+#include "metal/reflection.hpp"
+#include "metal/value.hpp"
+#include "metal/version.hpp"
