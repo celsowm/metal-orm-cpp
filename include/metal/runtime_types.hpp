c4a0d6526a60be114aed1cb9962bce208358e4d8
celsowm <celsowm@gmail.com>
2026-08-08 15:07:52 -0300

    refactor: expose lifecycle callbacks on tracked entities

--- include/metal/runtime_types.hpp
+++ include/metal/runtime_types.hpp
@@ -19,15 +19,22 @@ struct TrackedEntity {
     std::unordered_map<std::string, Value> original;
     std::function<std::unordered_map<std::string, Value>()> snapshot;
     std::function<void(const std::unordered_map<std::string, Value>&)> restore_snapshot;
-    std::function<std::function<void()>()> capture_relation_restore;
+    std::function<std::function<void()>()> capture_runtime_restore;
     std::function<Value()> get_pk;
     std::function<void(const Value&)> set_pk;
     bool generated_pk{false};
     std::function<void()> register_identity;
     std::function<void(const Value&)> erase_identity;
+    std::function<void()> before_insert;
+    std::function<void()> after_insert;
+    std::function<void()> before_update;
+    std::function<void()> after_update;
+    std::function<void()> before_delete;
+    std::function<void()> after_delete;
     std::function<void()> prepare_relations;
     std::function<void()> flush_relations;
     std::function<void()> accept_relations;
+    std::function<void()> dispatch_domain_events;
 };
 
 } // namespace metal
