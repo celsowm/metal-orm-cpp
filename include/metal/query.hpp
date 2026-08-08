73227434711fa84c8d02268e3216b8715947c147
celsowm <celsowm@gmail.com>
2026-08-08 13:32:12 -0300

    feat: expose relation and pagination query helpers

--- include/metal/query.hpp
+++ include/metal/query.hpp
@@ -4,3 +4,5 @@
 #include "metal/query/expressions.hpp"
 #include "metal/query/functions.hpp"
 #include "metal/query/select.hpp"
+#include "metal/query/relation_queries.hpp"
+#include "metal/query/pagination.hpp"
