dd760568a743d5531bea6066e1cb81c8b7b58f02
celsowm <celsowm@gmail.com>
2026-08-08 17:23:36 -0300

    feat: teach reflection about typed relation references

---
+++ include/metal/reference_traits.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "metal/reference.hpp"
+#include "metal/reflection.hpp"
+
+namespace metal::reflect {
+
+template <typename Target>
+struct single_relation_traits<metal::belongs_to_reference<Target>> {
+    static constexpr bool value = true;
+    using target_type = Target;
+};
+
+template <typename Target>
+struct single_relation_traits<metal::has_one_reference<Target>> {
+    static constexpr bool value = true;
+    using target_type = Target;
+};
+
+} // namespace metal::reflect
