bc856f4daa604d8c3e4aabc7c618926c9312e24e
celsowm <celsowm@gmail.com>
2026-08-09 12:55:29 -0300

    feat(openapi): expose reflected Tree schemas

--- include/metal/metal.hpp
+++ include/metal/metal.hpp
@@ -29,6 +29,7 @@
 #include "metal/schema_expected.hpp"
 #include "metal/schema_diff.hpp"
 #include "metal/tree.hpp"
+#include "metal/openapi_tree.hpp"
 #include "metal/unit_of_work.hpp"
 #include "metal/value.hpp"
 #include "metal/version.hpp"
